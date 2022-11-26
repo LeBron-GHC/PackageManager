159e00847f33961140141c4f82e6ee4c4dd6b5ba
LeBron-GHC <79261042+LeBron-GHC@users.noreply.github.com>
2022-11-26 21:22:30 +0800

    Add files via upload

---
+++ com.ghc.demotest/Runtime/Test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GHC 
+{
+    public static class Test
+    {
+        public static void Start()
+        {
+            Debug.Log("Test");
+        }
+    }
+}
+
